f15b484f2b4d60da488051d1acf5c0b0190c0402
Ali Sheraz <ali.sheraz@lhr.oneclout.com>
2024-10-05 12:12:00 +0500

    Add: Passing Data with Props

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,8 +1,29 @@
-export default function Page() {
+export default async function Page() {
+
+  async function getData() {
+    const res = await fetch("https://snowtooth-api-rest.fly.dev");
+    return res.json();
+
+  }
+
+  const data = await getData()
+
+  const HotelName = ({ name }: { name: string }) => {
+    return (
+      <h2 className="text-lg font-bold text-center sm:text-left">
+        {name}
+      </h2>
+    );
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        More react to come
+        {data.map((hotel: { id: string; name: string }) => (
+          <div key={hotel.id} className="w-full">
+            <HotelName name={hotel.name} />
+          </div>
+        ))}
       </main>
     </div>
   );
