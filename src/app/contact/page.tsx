c00bc1ed1321dd5008c6c46c371d46f0b9298852
Ali Sheraz <ali.sheraz@lhr.oneclout.com>
2024-10-07 12:12:00 +0500

    Add: Building form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,47 @@ export default function Page() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Contact Us!</h1>
+        <h1 className="text-2xl font-bold text-center mb-6">
+          Contact us!
+        </h1>
+        <form className="space-y-4">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              required
+              className="border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="message"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              required
+              name="message"
+              rows={4}
+              className="border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-600 rounded-md p-3"
+          >
+            Send Message
+          </button>
+        </form>
       </main>
     </div>
   );
